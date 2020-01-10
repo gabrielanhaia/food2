3ffab3f117475f3aefec9000e6e50da69c0b1019
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-10 23:51:51 +0000

    feature: Create table answers

---
+++ src/database/migrations/2020_01_10_234719_create_answers_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateAnswersTable
+ *
+ * Migration responsible for creating table "answers"
+ */
+class CreateAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('question_id')->unsigned()->nullable(false);
+            $table->text('valid_value', 256);
+            $table->timestamps();
+            $table->softDeletes()->index('index_answers_deleted_at');
+        });
+
+        Schema::table('answers', function (Blueprint $table) {
+            $table->foreign('question_id')
+                ->references('id')->on('questions')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
