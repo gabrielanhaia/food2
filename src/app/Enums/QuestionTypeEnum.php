80bf2ad3f77c38e037bf6bc60cc71509878bba48
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-10 23:27:20 +0000

    feature: Create enum with for form types
    Create class QuestionTypeEnum to keep the code organized

---
+++ src/app/Enums/QuestionTypeEnum.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Enums;
+
+use Eloquent\Enumeration\AbstractEnumeration;
+
+/**
+ *
+ * @package App\Enums
+ *
+ * @method static $this NUMBER()
+ * @method static $this TEXT()
+ * @method static $this DATE()
+ * @method static $this RADIO()
+ * @method static $this DROPDOWN()
+
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class QuestionTypeEnum extends AbstractEnumeration
+{
+    /** @var string NUMBER Type number. */
+    const NUMBER = 'number';
+
+    /** @var string TEXT Type text. */
+    const TEXT = 'text';
+
+    /** @var string DATE Type date. */
+    const DATE = 'date';
+
+    /** @var string RADIO Type radio. */
+    const RADIO = 'radio';
+
+    /** @var string DROPDOWN Type dropdown. */
+    const DROPDOWN = 'dropdown';
+}
