851c2f0e123585119e5a8b5cfcb234879743973c
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-11 00:02:21 +0000

    feature: Create table user_answers

---
+++ src/database/migrations/2020_01_10_235709_create_user_answers_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUserAnswersTable
+ *
+ * Migration responsible for creating table "user_answers"
+ */
+class CreateUserAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->bigInteger('user_id')->unsigned()->nullable(false);
+            $table->bigInteger('question_id')->unsigned()->nullable(false);
+            $table->bigInteger('answer_id')->unsigned()->nullable(false);
+            $table->text('text_answer');
+            $table->date('date_answer');
+            $table->float('number_answer');
+            $table->timestamps();
+            $table->softDeletes()->index('index_user_answers_deleted_at');
+
+            $table->primary(['user_id', 'question_id', 'answer_id']);
+        });
+
+        Schema::table('user_answers', function (Blueprint $table) {
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
+
+            $table->foreign('answer_id')
+                ->references('id')->on('answers')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
+
+            $table->foreign('question_id')
+                ->references('id')->on('questions')
+                ->onUpdate('NO ACTION')
+                ->onDelete('NO ACTION');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_answers');
+    }
+}
