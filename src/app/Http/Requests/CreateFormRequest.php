28c2ad28a0764f74ad5d884ebff700415c20bb5d
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-01-11 18:07:34 +0000

    feature: Create resource and responses for create form endpoint

---
+++ src/app/Http/Requests/CreateFormRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\QuestionTypeEnum;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class responsible for validating the requests to create a new form.
+ *
+ * @package App\Http\Requests
+ */
+class CreateFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required|integer',
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'introduction' => 'nullable|string',
+            'start_publish' => 'nullable|date_format:Y-m-d',
+            'end_publish' => 'nullable|date_format:Y-m-d',
+            'questions' => 'required|array',
+            'questions.*.description' => 'required|string',
+            'questions.*.mandatory' => 'required|boolean',
+            'questions.*.type' => 'required|' . QuestionTypeEnum::formValidationString()];
+    }
+
+
+}
---
+++ src/app/Http/Requests/CreateQuestionRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\QuestionTypeEnum;
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class CreateQuestionRequest
+ * @package App\Http\Requests
+ *
+ * @author Gabriel Anhaia <anhaia.gabriel@gmail.com>
+ */
+class CreateQuestionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'form_id' => 'required|integer',
+            'description' => 'required|string',
+            'mandatory' => 'required|boolean',
+            'type' => 'required|' . QuestionTypeEnum::formValidationString()
+        ];
+    }
+}
